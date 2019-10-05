c0ad6d9fb07c8ab76500f0f255e8e823acc5bc41
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-05 07:02:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,12 +10,5 @@ def map(array)
 end
 
 def reduce(array, start = 0)
-  result = array[0]
-  
-  array.drop(start).each do |a|
-    result = yield(a)
-    puts result
-  end
-  
-  return result
+  puts array
 end  
